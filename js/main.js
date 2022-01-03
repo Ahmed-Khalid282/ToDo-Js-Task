f2233081e0e9f0a5dfa2fee91dabb8a79e01f571
Ahmed-Khalid282 <akh28292@gmail.com>
2022-01-03 11:58:24 +0200

    first js task

--- js/main.js
+++ js/main.js
@@ -1,28 +1,72 @@
 let btn = document.getElementById('add');
 let input = document.getElementById('maininput');
 let nodata = document.getElementById('nodata');
-let taskdata = document.getElementById('taskdata');
-let pp = document.getElementById('pp');
-let del = document.getElementById('delete')
+let tasks = document.getElementById('taskdata');
 
 
-let show = () =>{
+let addtask = () => {
     let task = input.value
-    if(!task){
-        alert('You must enter valid data');
-    }else{
-        pp.innerText = task;
-        taskdata.style.display = 'flex';
-        nodata.style.display = 'none'
+
+
+    if (!task) {
+        alert('You must enter a valid date');
+        return;
     }
+
+    nodata.classList.add('none')
+
+    let taskdata = document.createElement('div')
+    taskdata.classList = 'taskdata'
+
+    let inputdata = document.createElement('input')
+    inputdata.type = 'text'
+    inputdata.classList = 'inputdata'
+    inputdata.value = task
+    taskdata.append(inputdata)
+    inputdata.setAttribute('readonly', 'readonly')
+
+    let delbtn = document.createElement('button')
+    delbtn.classList = 'delbutton'
+    delbtn.innerText = 'Delete'
+    taskdata.append(delbtn)
+
+    let edbtn = document.createElement('button')
+    edbtn.classList = 'editebutton'
+    edbtn.innerText = 'Edite'
+    taskdata.append(edbtn)
+
+    tasks.append(taskdata)
+
+    let edite = () => {
+        if (edbtn.innerText.toUpperCase() == "EDITE") {
+            inputdata.removeAttribute('readonly')
+            inputdata.focus()
+            edbtn.innerText = "Save"
+            edbtn.style.backgroundColor='#1e831e'
+        } else {
+            inputdata.setAttribute('readonly', 'readonly')
+            edbtn.innerText = 'Edite'
+            edbtn.style.backgroundColor='rgb(157 143 62)'
+        }
+    }
+
+    edbtn.addEventListener('click', edite)
+
+    let deletee = () => {
+        tasks.removeChild(taskdata)
+        if (tasks.childElementCount == 0) {
+            nodata.classList.remove('none')
+        }
+    }
+
+
+    delbtn.addEventListener('click', deletee)
+
+    input.value = ''
+
+
+
+
 }
 
-btn.addEventListener('click' , show)
-
-
-let remove = () =>{
-    taskdata.style.display = 'none';
-    nodata.style.display = 'block'
-}
-
-del.addEventListener('click' , remove)
+btn.addEventListener('click', addtask)
